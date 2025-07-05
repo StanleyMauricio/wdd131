fc1c55a1043d1edb00319fc021307a7509b13ef2
stanleymauricio <stanleymauricio.sm@gmail.com>
2025-07-05 17:12:09 -0500

    added

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,10 +1,23 @@
-const currentYearSpan = document.getElementById('currentyear');
+// seleccionar los elementos del DOM
+const currentYearSpan = document.querySelector("#currentyear");
+const lastModifiedParagraph = document.querySelector("#lastModified");
+
+// usar objeto Date
+const today = new Date();
+
+// poner año actual
 if (currentYearSpan) {
-    currentYearSpan.textContent = new Date().getFullYear();
+	currentYearSpan.textContent = today.getFullYear();
 }
 
-
-const lastModifiedParagraph = document.getElementById('lastModified');
+// poner última modificación con formato legible
 if (lastModifiedParagraph) {
-    lastModifiedParagraph.textContent = "Last Modified: " + document.lastModified;
+	const modified = new Date(document.lastModified);
+
+	const formatted = new Intl.DateTimeFormat("en-US", {
+		dateStyle: "full",
+		timeStyle: "medium"
+	}).format(modified);
+
+	lastModifiedParagraph.innerHTML = `Last Modified: <span class="highlight">${formatted}</span>`;
 }
